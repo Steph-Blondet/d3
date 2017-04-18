f1349baa079cdd6bcdcbbf35c21f7d5df36738ee
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-04-17 21:22:22 -0400

    setting random background color

--- circle-dragging-I/app.js
+++ circle-dragging-I/app.js
@@ -1,10 +1,12 @@
 console.log('hello?');
 
+//grabbing elements
 var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    radius = 32;
+    width = svg.attr("width"),
+    height = svg.attr("height"),
+    radius = 40;
 
+//creating 20 circles
 var circles = d3.range(20).map(function() {
   return {
     x: Math.round(Math.random() * (width - radius * 2) + radius),
@@ -12,9 +14,16 @@ var circles = d3.range(20).map(function() {
   };
 });
 
+//setting random colors
 var color = d3.scaleOrdinal()
     .range(d3.schemeCategory20);
 
+//setting background color to a random color
+svg.style("background-color", function(){
+  return "hsl(" + 360 * Math.random() + ",100%,95%)";
+});
+
+//creating circles
 svg.selectAll("circle")
   .data(circles)
   .enter().append("circle")
@@ -27,6 +36,7 @@ svg.selectAll("circle")
         .on("drag", dragged)
         .on("end", dragended));
 
+//drag listeners
 function dragstarted(d) {
   d3.select(this).raise().classed("active", true);
 }

dc8bdec1ad3099b158d8728574f420e621d61312
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-05-31 22:17:52 -0400

    updating data file, creating buttons to add functionality, and creating combine and split functionalities

--- bubble-chart/js/app.js
+++ bubble-chart/js/app.js
@@ -2,8 +2,8 @@ console.log('app is connected!');
 
 (function() {
   //setting the width and height of our board
-  var width = 500;
-      height = 500;
+  var width = 900;
+      height = 600;
 
   //creating the svg and appending it to the id
   var svg = d3.select("#bubble-chart")
@@ -16,14 +16,26 @@ console.log('app is connected!');
   var radiusScale = d3.scaleSqrt().domain([1, 100]).range([20, 50])
 
   //collection of forces about where we want our cicles to go and how we want them to interact
+  var forceXSplit = d3.forceX(function(d) {
+    if (d.Category === 'Development') {
+      return 250
+    } else {
+      return 700
+    }
+  }).strength(0.05)
+
+  var forceXCombine = d3.forceX(width / 2).strength(0.05)
+
+  var forceCollide = d3.forceCollide(function(d) {
+    return radiusScale(d.Level) + 1;
+  })
+
   var simulation = d3.forceSimulation()
     //telling the circles to go to the middle of the board
-    .force("x", d3.forceX(width / 2).strength(0.05))
+    .force("x", forceXCombine)
     .force("y", d3.forceY(height / 2).strength(0.05))
     //making circles to not collide. give the radius of the area that we want the collision to avoid.
-    .force("collide", d3.forceCollide(function(d) {
-      return radiusScale(d.Level) + 2;
-    }))
+    .force("collide", forceCollide)
 
   //queuing our data
   d3.queue()
@@ -43,6 +55,20 @@ console.log('app is connected!');
       .on("click", function(d) {
         console.log(d);
       })
+
+    d3.select("#split").on("click", function() {
+      simulation
+        .force("x", forceXSplit)
+        .alphaTarget(0.35)
+        .restart()
+    })
+
+    d3.select("#combine").on("click", function() {
+      simulation
+        .force("x", forceXCombine)
+        .alphaTarget(0.35)
+        .restart()
+    })
 
     //tells the browser to run the 'ticked' function
     simulation.nodes(datapoints)
